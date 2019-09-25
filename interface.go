67b137124b5d006240bcd9cc77a28deb7dd5d9be
shumon84 <cameremon84@gmail.com>
2019-09-25 16:36:41 +0900

    Add binutil.Writer interface

--- interface.go
+++ interface.go
@@ -18,3 +18,22 @@ type Reader interface {
 	String() (string, error)
 	Strings(n int) (data []string, err error)
 }
+
+// Writer is binary write utility.
+// Note this interface is not io.Writer.
+type Writer interface {
+	Write(data interface{}) error
+	Seek(offset int64, whence int) (int64, error)
+	UInt8(data uint8) error
+	UInt8s(data []uint8) error
+	UInt16(data uint16) error
+	UInt16s(data []uint16) error
+	UInt32(data uint32) error
+	UInt32s(data []uint32) error
+	UInt64(data uint64) error
+	UInt64s(data []uint64) error
+	Byte(data byte) error
+	Bytes(data []byte) error
+	String(data string) error
+	Strings(data []string) error
+}
