7a81d19f7a1d8a940f2bb1883123df4ce5448e74
shumon84 <cameremon84@gmail.com>
2019-09-25 12:45:21 +0900

    Add binutil.Reader interface

---
+++ interface.go
@@ -0,0 +1,18 @@
+package binutil
+
+type Reader interface {
+	Read(data interface{}) error
+	Seek(offset int64, whence int) (int64, error)
+	UInt8() (data uint8, err error)
+	UInt8s(n int) (data []uint8, err error)
+	UInt16() (data uint16, err error)
+	UInt16s(n int) (data []uint16, err error)
+	UInt32() (data uint32, err error)
+	UInt32s(n int) (data []uint32, err error)
+	UInt64() (data uint64, err error)
+	UInt64s(n int) (data []uint64, err error)
+	Byte() (data byte, err error)
+	Bytes(n int) (data []byte, err error)
+	String() (string, error)
+	Strings(n int) (data []string, err error)
+}
